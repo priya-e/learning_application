14dc64ee93a1a1e69a6cc14013443847993ed157
priya-e <priyankamaran.e24@gmail.com>
2022-06-28 04:46:59 +0000

    [ADD] Basic Module

---
+++ Academy/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{'name': 'Learning Academy',
+ 'summary':"""Academy application to manage training""".
+ 'description':"""
+ Academy module to manage training
+ - Courses
+ - Sessions
+ - Attendees
+ """,
+ 'author' : 'Odoo',
+ 'website': 'https://www.odoo.com',
+ 'category':'training',
+ 'version' : '0.1',
+ 'depends': ['base'],
+ 'data' : [],
+ 'demo' : [],
+ 'license' : 'gpl3'
+
+}
